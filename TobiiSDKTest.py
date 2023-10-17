8c3df6bd2eae7bb56a55d4bbf96ddb2b310bd9c3
Willem Hueffed <whueffed@seattleu.edu>
2023-10-17 16:55:09 -0700

    Modifies gaze_data with new gaze_data Modifies TobiiSDKTest.py to write the file in better json formatting

--- TobiiSDKTest.py
+++ TobiiSDKTest.py
@@ -3,6 +3,10 @@ import tobii_research as tr
 import time
 import json
 
+#TODO: 1. __name__==__main__ 2. Add filtering to replace NaN with null
+#3. figure out way to r/w to a common buffer
+#4. write script to determine fixation locations
+#5. After above is completed, write master script to create threads for each -> they should run in ||
 
 found_eyetrackers = tr.find_all_eyetrackers()
 
@@ -12,17 +16,19 @@ print("Model: " + my_eyetracker.model)
 print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
 print("Serial number: " + my_eyetracker.serial_number)
 
+gazeDataEntries=[] #global scope so calling function can call it back
+
 def gaze_data_callback(gaze_data):
-    print(gaze_data)
-    with open("gaze_data.json", "a") as output:
-        output.write(json.dumps(gaze_data) + "\n")
-    # Print gaze points of left and right eye
-    # print("Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
-    #     gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
-    #     gaze_right_eye=gaze_data['right_gaze_point_on_display_area']))
-    
+    gazeDataEntries.append(gaze_data)
+
 my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
-
 time.sleep(1)
-
 my_eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
+
+
+with open("gaze_data.json", "w") as output:
+    output.write("[\n")
+    for i in range(0, len(gazeDataEntries)-1):
+        output.write("\t" + json.dumps(gazeDataEntries[i]) + ",\n")
+    output.write("\t" + json.dumps(gazeDataEntries[i]) + "\n")
+    output.write("]\n")
