1939deae6c0880147931365a2baa6caf37b85436
Willem <willmhueffed@gmail.com>
2024-02-27 20:15:23 -0600

    gesture control works with turtlesim

--- gesture_control_ros/src/gesture_control/gesture_control/command_translator.py
+++ gesture_control_ros/src/gesture_control/gesture_control/command_translator.py
@@ -1,36 +1,40 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 class CommandTranslator(Node):
 
     def __init__(self):
         super().__init__('command_translator')
-        self.publisher = self.create_publisher(String, 'place_holder_topic', 10)
+        self.publisher = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
         self.subscriber = self.create_subscription(String, 'recognized_gesture', self.listener_callback, 10)
         #timer_period = 0.5
         #self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def listener_callback(self, msg):
-        output_msg = String()
-        self.get_logger().info('hearing: {}'.format(msg.data))
+        twist_msg = Twist()
+        #self.get_logger().info('hearing: {}'.format(msg.data))
         if msg.data == 'None':
-            output_msg.data = 'Hello world1'
+            pass
         if msg.data == 'Open_Palm':
-            output_msg.data = 'Hello world2'
-        if msg.data == 'Thumb_Up':
-            output_msg.data = 'Hello world3'
-        if msg.data == 'Thumb_Down':
-            output_msg.data = 'Hello world4'
+            twist_msg.angular.z = 1.0
         if msg.data == 'Closed_Fist':
-            output_msg.data = 'Hello world5'
+            twist_msg.angular.z = -1.0
+        if msg.data == 'Thumb_Up':
+            twist_msg.linear.x = 1.0
+        if msg.data == 'Thumb_Down':
+            twist_msg.linear.x = -1.0
+        if msg.data == 'Victory':
+            twist_msg.linear.y = 1.0
         if msg.data == 'Pointing_Up':
-            output_msg.data = 'Hello world6'
+            twist_msg.linear.y = -1.0  
         if msg.data == 'I_Love_You':
-            output_msg.data = 'Hello world7'
+            pass
         else:
-            output_msg.data = 'Error in listener_callback - command_recognizer'
-        self.publisher.publish(output_msg)
+            pass
+            #self.get_logger().info('Error in listener_callback - command_recognizer')
+        self.publisher.publish(twist_msg)
         #self.get_logger().info("I heard: {})".format(msg))
 
 def main(args=None):
