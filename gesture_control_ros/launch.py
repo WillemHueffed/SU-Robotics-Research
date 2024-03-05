8fa6acd4831f43b797d157948d84b5ff7e8cf41a
Willem <willmhueffed@gmail.com>
2024-03-05 11:56:12 -0800

    updates launch.py

--- gesture_control_ros/launch.py
+++ gesture_control_ros/launch.py
@@ -4,11 +4,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim',
-            executable='turtlesim_node',
-            name='sim'
-            ),
-        Node(
             package='gesture_control',
             executable='camera_feed',
             name='camera_feed'
