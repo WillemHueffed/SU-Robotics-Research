a01bee51755a327512fb174ae1bfdb1fbd6f1099
Willem <willmhueffed@gmail.com>
2024-04-03 13:22:38 -0700

    depth node now uses json formatting for messages

--- ros_ws/src/rgbd_camera/rgbd_camera/depth_node.py
+++ ros_ws/src/rgbd_camera/rgbd_camera/depth_node.py
@@ -4,13 +4,15 @@ from std_msgs.msg import String
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import time
+
 
 class RealSenseNode(Node):
     def __init__(self):
-        super().__init__('realsense_node')
+        super().__init__("realsense_node")
 
-        #Create a means of publishing
-        self.publisher_ = self.create_publisher(String, 'camera_coords', 10)
+        # Create a means of publishing
+        self.publisher_ = self.create_publisher(String, "camera_coords", 10)
 
         # Initialize global variables for clicked coordinates
         self.clicked_x = 0.5
@@ -24,8 +26,8 @@ class RealSenseNode(Node):
         self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
         self.pipeline.start(self.config)
-        
-        #Align depth frames to color frames
+
+        # Align depth frames to color frames
         self.align_to = rs.stream.color
         self.align = rs.align(self.align_to)
 
@@ -33,18 +35,18 @@ class RealSenseNode(Node):
         frames = self.pipeline.wait_for_frames()
         aligned_frames = self.align.process(frames)
 
-        #Extract frame data
+        # Extract frame data
         color_frame = aligned_frames.get_color_frame()
         color_image = np.asanyarray(color_frame.get_data())
-        
+
         depth_frame = aligned_frames.get_depth_frame()
         depth_image = np.asanyarray(depth_frame.get_data())
 
         # Set up the mouse callback function
-        cv2.namedWindow('Color', cv2.WINDOW_AUTOSIZE)
-        cv2.setMouseCallback('Color', self.mouse_callback, color_image)
-        cv2.namedWindow('Depth', cv2.WINDOW_AUTOSIZE)
-        cv2.setMouseCallback('Depth', self.mouse_callback, depth_image)
+        cv2.namedWindow("Color", cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback("Color", self.mouse_callback, color_image)
+        cv2.namedWindow("Depth", cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback("Depth", self.mouse_callback, depth_image)
 
     def mouse_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
@@ -70,28 +72,42 @@ class RealSenseNode(Node):
 
                 # Cast frames to image format via numpy
                 depth_image = np.asanyarray(depth_frame.get_data())
-                depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+                depth_colormap = cv2.applyColorMap(
+                    cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET
+                )
                 color_image = np.asanyarray(color_frame.get_data())
 
                 # Get the depth data from pos x,y | flip the y value via 1-y -> depth frame y coords is opposite eye tracker
-                xyDepth = depth_frame.get_distance(int(self.clicked_x * depth_frame.width), int((1 - self.clicked_y) * depth_frame.height))
-                #print("Clicked at position: ({}, {}) depth is: {}".format(self.clicked_x, self.clicked_y, xyDepth))
-                
+                xyDepth = depth_frame.get_distance(
+                    int(self.clicked_x * depth_frame.width),
+                    int((1 - self.clicked_y) * depth_frame.height),
+                )
+                # print("Clicked at position: ({}, {}) depth is: {}".format(self.clicked_x, self.clicked_y, xyDepth))
+
                 # Point cloud
                 x_pxl = int(self.clicked_x * depth_frame.width)
                 y_pxl = int((1 - self.clicked_y) * depth_frame.height)
-                depth_intrinsics = depth_frame.profile.as_video_stream_profile().intrinsics
-                depth_point = rs.rs2_deproject_pixel_to_point(depth_intrinsics, [x_pxl, y_pxl], xyDepth)
+                depth_intrinsics = (
+                    depth_frame.profile.as_video_stream_profile().intrinsics
+                )
+                depth_point = rs.rs2_deproject_pixel_to_point(
+                    depth_intrinsics, [x_pxl, y_pxl], xyDepth
+                )
 
-                #publish message
+                # publish message
                 msg = String()
-                msg.data = "{} {} {} {}".format(self.clicked_x, self.clicked_y, xyDepth, depth_point)
+                msg.data = "screen_coords: {}, world_coords: {}".format(
+                    (self.clicked_x, self.clicked_y), depth_point
+                )
+                msg.data = "{" + msg.data + "}"
                 self.publisher_.publish(msg)
-                self.get_logger().info('Publishing: {}'.format(msg.data))
-                
+                self.get_logger().info("Publishing: {}".format(msg.data))
+
                 # Display the color image in the window
-                cv2.imshow('Color', color_image)
-                cv2.imshow('Depth', depth_colormap)
+                cv2.imshow("Color", color_image)
+                cv2.imshow("Depth", depth_colormap)
+
+                time.sleep(0.100)  # Sleep for 100 ms
 
                 cv2.waitKey(1)
         finally:
@@ -106,5 +122,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
